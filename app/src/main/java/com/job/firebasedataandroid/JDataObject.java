3066720adb9dfaa596a829b0fa45c8dfda81ff3c
JobM <jobmwaniki18@gmail.com>
2015-11-12 22:57:44 +0300

    initial commit

---
+++ app/src/main/java/com/job/firebasedataandroid/JDataObject.java
@@ -0,0 +1,47 @@
+package com.job.firebasedataandroid;
+
+/**
+ * Created by Job on 11/12/2015.
+ */
+public class JDataObject {
+    int id;
+    String company_name;
+    String country;
+    String credit_card;
+    String email;
+    String first_name;
+    String last_name;
+
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCredit_card() {
+        return credit_card;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public JDataObject(){
+
+    }
+}
---
+++ app/src/main/java/com/job/firebasedataandroid/MainActivity.java
@@ -0,0 +1,94 @@
+package com.job.firebasedataandroid;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    static Object res;
+    JDataObject object;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getData("https://hackfest.firebaseio.com/hackfest");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getData(String url){
+        Firebase ref = new Firebase(url);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                for (DataSnapshot data:snapshot.getChildren()) {
+                    object = data.getValue(JDataObject.class);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+
+    }
+
+
+}
